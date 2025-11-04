c76af1afda9d76b0dd53959d8b739811fdb9e966
benedictojal-coder <benedictojal@gmail.com>
2025-11-04 22:31:39 +0300

    Add root-level logger middleware to log method, path, and IP

--- myApp.js
+++ myApp.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 let express = require("express");
 let app = express();
 
+function loggerMiddleware(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+
+app.use(loggerMiddleware);
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
