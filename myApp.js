1238f7edb5a1678996a11c423a5c78d813e5972e
benedictojal-coder <benedictojal@gmail.com>
2025-11-05 11:59:16 +0300

    Add body-parser and /name POST route

--- myApp.js
+++ myApp.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/json", function (req, res) {
+app.get("/json", (req, res) => {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
@@ -21,20 +23,27 @@ function addCurrentTime(req, res, next) {
   next();
 }
 
-app.get("/now", addCurrentTime, function (req, res) {
+app.get("/now", addCurrentTime, (req, res) => {
   res.json({ time: req.time });
 });
 
-app.get("/:word/echo", function (req, res) {
+app.get("/:word/echo", (req, res) => {
   const word = req.params.word;
   res.json({ echo: word });
 });
 
-app.get("/name", function (req, res) {
-  const firstName = req.query.first;
-  const lastName = req.query.last;
-  const fullName = `${firstName} ${lastName}`;
-  res.json({ name: fullName });
-});
+app.route("/name")
+  .get((req, res) => {
+    const firstName = req.query.first;
+    const lastName = req.query.last;
+    const fullName = `${firstName} ${lastName}`;
+    res.json({ name: fullName });
+  })
+  .post((req, res) => {
+    const firstName = req.body.first;
+    const lastName = req.body.last;
+    const fullName = `${firstName} ${lastName}`;
+    res.json({ name: fullName });
+  });
 
 module.exports = app;
